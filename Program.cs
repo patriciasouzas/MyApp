fcd91767de1862c6a206d28f344634ab8917e930
patriciasouzas <patriciasouzaa89@gmail.com>
2023-04-04 19:47:16 -0300

    interpolação

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 {
     static void Main(string[] args)
     {
+        Interpolacao();
+    }
+
+    static void Interpolacao()
+    {
         var price = 10.2;
         // var texto = ("O preço do produto é " + price + " apenas na promoção");
         // var texto = string.Format("O preço do produto é {0} apenas na promoção", price);
