c3be6703cc4ee5267c5148be75f8247a850202b4
patriciasouzas <patriciasouzaa89@gmail.com>
2023-04-04 19:27:55 -0300

    added changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var id = Guid.NewGuid();
+        id.ToString();
+
+        id = new Guid("7a6b45ba-06da-400b-85c7-ce38048fca2f");
+        Console.WriteLine(id.ToString().Substring(0, 8));
+
+        id = Guid.NewGuid();
+        Console.WriteLine(id.ToString().Substring(0, 8));
+    }
+}
